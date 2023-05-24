bce020c8f49d1ba5524f4d9ffade4596c009429c
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-24 16:48:43 -0300

    a

--- app.py
+++ app.py
@@ -4,25 +4,28 @@
 #pip install flask
 
 from flask import Flask, make_response, json, jsonify
-from mock import Seed
+from feedback import Feedbacks
 
 app = Flask(__name__)
 #this prevent the response return in alphabetical order
+
 app.config['JSON_SORT_KEYS'] = False
+
 
 #_____________________________________________________________________________
 
-@app.route("/v1/allseeds",methods=['GET'])
+@app.route("/v1/feedbacks",methods=['GET'])
 def a() :
     return make_response(
         jsonify(
             
               message='200',
-              seed_data = Seed
+              data = Feedbacks
                 
         )
     )
     
+
 
 #_____________________________________________________________________________
 
---
+++ feedback.py
@@ -0,0 +1,41 @@
+Feedbacks = [
+                { 
+                
+                "id":"1",
+                "nome":"Caio palermo cogh lemos",
+                "rate":"ADOREI",
+                "feedback":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at finibus diam. Nulla efficitur neque eget est lacinia, pellentesque rhoncus velit porta. Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.",
+                "date":"03/07/2004",
+                "img":"https://static.wikia.nocookie.net/gintama/images/a/a2/Screen-6.jpg/revision/latest/scale-to-width-down/1200?cb=20200203021409&path-prefix=pt"
+                
+                },
+                { 
+                "id":"2",
+                "nome":"Camila alves pinheiro",
+                "rate":"GOSTEI MUITO",
+                "feedback":"Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.",
+                "date":"24/02/2004",
+                "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUka28vE7Cwaupbl9x88G91rKelEGBhcdIRw&usqp=CAU"
+                },
+                 { 
+                "id":"3",
+                "nome":"Giovana santos",
+                "rate":"FOI OK",
+                "feedback":"Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.Etiam dapibus tempus tellus et volutpat. Curabitur commodo enim bibendum, condimentum augue id, elementum magna.",
+                "date":"03/10/2003",
+                "img":"https://i.pinimg.com/236x/15/91/61/159161b555d581c1a42b4210a175439c.jpg"
+                },
+                 { 
+                "id":"3",
+                "nome":"Paula ",
+                "rate":"NAO GOSTEI",
+                "feedback":"Curabitur commodo enim bibendum, condimentum augue id, elementum magna.",
+                "date":"19/03/2003",
+                "img":"https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg"
+                },
+                
+         
+             ]
+           
+        
+
