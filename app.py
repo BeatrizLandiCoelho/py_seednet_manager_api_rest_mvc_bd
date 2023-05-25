22fa83b93f8ddd7844834afd1a79dfabb08a2e54
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-24 21:14:49 -0300

    a

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, make_response, json, jsonify
 from feedback import Feedbacks
+from companies import CompaniesData
 
 app = Flask(__name__)
 #this prevent the response return in alphabetical order
@@ -15,7 +16,7 @@ app.config['JSON_SORT_KEYS'] = False
 #_____________________________________________________________________________
 
 @app.route("/v1/feedbacks",methods=['GET'])
-def a() :
+def peoples_feedbacks() :
     return make_response(
         jsonify(
             
@@ -25,6 +26,15 @@ def a() :
         )
     )
     
+@app.route("/v1/companie",methods=['GET'])
+def companies_friends():
+    return make_response(
+        jsonify(
+            
+            message = '200',
+            data = CompaniesData
+        )
+    )
 
 
 #_____________________________________________________________________________
@@ -33,3 +43,5 @@ if __name__ == '__main__':
 
     print("server read to go")
     app.run(debug=True, port=8080)
+
+    #pip freeze > "requirements.txt"
